295660c3a1d60a8f653628e120b45e3cb226acba
pietergmail <33780619+pietergmail@users.noreply.github.com>
2021-08-05 16:38:29 +0200

    added bootstrap

---
+++ deelopdracht_3/src/main/java/controller/deletejson.java
@@ -0,0 +1,28 @@
+package controller;
+
+import com.google.gson.Gson;
+import domain.Person;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class deletejson extends RequestHandler{
+    @Override
+    public void handleRequest(HttpServletRequest request, HttpServletResponse response) throws IllegalAccessException, ServletException, IOException {
+
+        InputStream in = request.getInputStream();
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        String data = br.readLine();
+
+        Gson gson = new Gson();
+
+        Person p = gson.fromJson(data, Person.class);
+
+        persons.delete(p.getFirstName());//remove first person from list
+    }
+}
